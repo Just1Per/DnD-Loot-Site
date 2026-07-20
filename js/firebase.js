81433ab5927a5aca739de5414829483c2ddd7cfe
Casper Nørlem <caspno@regsj.dk>
2026-07-20 14:37:13 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -13,13 +13,13 @@ import {
 from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDyj69qiEnkKk4lZjleGnvFEGUvLcSZIk",
-    authDomain: "dnd-loot-site.firebaseapp.com",
-    projectId: "dnd-loot-site",
-    storageBucket: "dnd-loot-site.firebasestorage.app",
-    messagingSenderId: "345641251366",
-    appId: "1:345641251366:web:081fe07bc2cc859901ace7",
-    measurementId: "G-ZHNPHYLDJ0"
+  apiKey: "AIzaSyDyj69qiEnkKk4lZjleGnvFEGUdvLcSZIk",
+  authDomain: "dnd-loot-site.firebaseapp.com",
+  projectId: "dnd-loot-site",
+  storageBucket: "dnd-loot-site.firebasestorage.app",
+  messagingSenderId: "345641251366",
+  appId: "1:345641251366:web:081fe07bc2cc859901ace7",
+  measurementId: "G-ZHNPHYLDJ0"
 };
 
 const app = initializeApp(firebaseConfig);
