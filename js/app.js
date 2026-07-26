e7abd5172ef9560cf8a44f7f344ef79fe72e54c5
Just1Per <103027400+Just1Per@users.noreply.github.com>
2026-07-26 22:17:02 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1063,7 +1063,7 @@ onAuthStateChanged(
 
             populateOwnerFilter();
 
-            //importItemsToFirestore();
+            importItemsToFirestore();
 
             await loadItemsFromFirestore();
 
